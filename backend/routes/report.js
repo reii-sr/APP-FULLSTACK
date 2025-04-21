a9d72cd7cb623b31d7d31980361fb5cb47a3cd26
reii-sr <rsr.reiise@gmail.com>
2025-04-21 00:03:03 -0500

    reestructurado

--- backend/routes/report.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const { getReport } = require('../controllers/reportController');
-
-// GET /api/report
-router.get('/report', getReport);
-
-module.exports = router;
